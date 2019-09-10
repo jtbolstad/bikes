30a4594bdfa7ba803bfa7f5a49875699fe772d3c
Jan Thure Bolstad <jtbolstad@gmail.com>
2019-09-10 22:53:04 +0200

    Proxy. Readme.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,30 @@ import "./App.css";
 function App() {
   const [data, setData] = useState({ stations: [], status: [] });
 
-  const handleFetchResponse = response => {
-    const hasError = !response.ok;
-    const isLoading = false;
-    return response.ok && response.json ? response.json() : data;
-  };
-
   useEffect(() => {
     async function fetchData() {
       try {
-        const apiUrls = [
-          "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json",
-          "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"
+        // Using CRA's proxy to avoid CORS issues (see package.json)
+        const apiEndpoints = [
+          "/oslobysykkel.no/station_information.json",
+          "/oslobysykkel.no/station_status.json"
         ];
+
+        const headers = {
+          // Identifying header ref API readme
+          headers: {
+            "client-name": "jt-bikeapp"
+          }
+        };
+
+        const handleFetchResponse = response =>
+          response.ok && response.json ? response.json() : null;
+
         const [stations, status] = await Promise.all(
-          apiUrls.map(url =>
-            fetch(url)
-              .then(res => res.json())
-              .then(res => res.data.stations)
+          apiEndpoints.map(url =>
+            fetch(url, headers)
+              .then(handleFetchResponse)
+              .then(response => response.data.stations)
           )
         );
         setData({ stations, status });
@@ -32,34 +38,21 @@ function App() {
     fetchData();
   }, []);
 
-  // USE THIS?
-  // https://gist.github.com/bschwartz757/5d1ff425767fdc6baedb4e5d5a5135c8
-  //   var requestAsync = function(url) {
-  //     return new Promise((resolve, reject) => {
-  //         var req = request(url, (err, response, body) => {
-  //             if (err) return reject(err, response, body);
-  //             resolve(JSON.parse(body));
-  //         });
-  //     });
-  // };
-
   const Row = ({ id }) => {
-    // TODO: Optimize this?
     const station = data.stations.find(station => station.station_id === id);
     const status = data.status.find(status => status.station_id === id);
 
-    console.log("station", station);
     return (
       <tr>
         <td>{station.name}</td>
         <td>{status.num_bikes_available}</td>
         <td>{status.num_docks_available}</td>
         <td>
+          {/* Just adding a map link for my own interest/curiosity */}
           <a
             href={`http://maps.google.com/maps?q=${station.lat}+${station.lon}&ll=${station.lat},${station.lon}&spn=0.004250,0.011579&t=h&iwloc=A&hl=en`}
-            target="_blank"
           >
-            Vis i kart
+            Vis
           </a>
         </td>
       </tr>
@@ -88,44 +81,3 @@ function App() {
 }
 
 export default App;
-
-// {/* href={`https://www.google.com/maps/@${station.lat},${station.lon},14z?hl=no`} */}
-
-// More verbose code:
-// const stationsResponse = await fetch(
-//   "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
-// );
-// const statusResponse = await fetch(
-//   "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"
-// );
-
-// // Bail out if any request failed:
-// if (!stationsResponse.ok || !statusResponse.ok) {
-//   console.log("Request failed:");
-//   console.log("Stations:" + stationsResponse.status);
-//   console.log("Status:" + statusResponse.status);
-//   return;
-// }
-
-// const stations = await stationsResponse.json();
-// const status = await statusResponse.json();
-
-// setData({
-//   stations: stations.data.stations,
-//   status: status.data.stations
-// });
-
-// console.log("stationsResponse", stationsResponse);
-// console.log("statusResponse", statusResponse);
-// const { stations } = await stationsResponse.json().data;
-// const { status } = await statusResponse.json().data;
-// console.log("stations", stations);
-// console.log("status", status);
-// {
-//   headers: {
-//     Accept: "application/json"
-//     // "Content-Type": "application/json"
-//   }
-// }
-
-//  console.log("data", data);
--- src/index.js
+++ src/index.js
@@ -10,36 +10,3 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-/*
-
-API
-    https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json
-    https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json
-
-Expo? https://snack.expo.io/@jtbolstaf/arrogant-french-fries
-
-Proxy for CORS
-    https://medium.com/@Pavan_/set-up-proxy-to-work-with-multiple-apis-in-create-react-app-be595a713eb2
-Readme
-
-Error handling
-    error boundary
-    if (!(status >= 200 && status < 300)) {
-
-A11y: ARIA, semantisk markup
-
-UseReducer?
-
-Security
-
-Structure:
-    Components/containers?
-
-Types?
-
-Map?
-
-Tests
-
-*/
