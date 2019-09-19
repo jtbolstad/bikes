a990836ddbb5e958831222e97ce7d33974afea75
Jan Thure Bolstad <jtbolstad@gmail.com>
2019-09-19 23:42:27 +0200

    Updates to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Kart from "./Kart.js";
+import BikeMap from "./BikeMap.js";
+
+let fetchData;
 
 const App = () => {
-  const [stationsWithStatus, setData] = useState(new Map());
+  const [stationsWithStatus, setStations] = useState(new Map());
   const [updateTime, setUpdateTime] = useState(new Date());
 
-  async function fetchData() {
+  fetchData = async () => {
     // Using CRA's proxy to avoid CORS (see package.json)
     const apiEndpoints = [
       "/oslobysykkel.no/station_information.json",
@@ -29,6 +31,7 @@ const App = () => {
         }
 
         const responseJson = await response.json();
+        // Both requests return data.stations
         return responseJson.data && responseJson.data.stations
           ? responseJson.data.stations
           : {};
@@ -42,8 +45,8 @@ const App = () => {
         ...status.find(status => station.station_id === status.station_id)
       });
     });
-    setData(stationsWithStatus);
-    setUpdateTime(new Date());
+    setStations(stationsWithStatus);
+    setUpdateTime(new Date().toTimeString().substr(0,8));
   }
 
   useEffect(() => {
@@ -52,43 +55,14 @@ const App = () => {
     return () => clearInterval(inter);
   }, []);
 
-  const dataTableRows = Array.from(stationsWithStatus).map(([id, station]) => (
-    <tr key={id}>
-      <td>{station.name}</td>
-      <td>{station.num_bikes_available}</td>
-      <td>{station.num_docks_available}</td>
-      <td>
-        {/* Adding a map link for my own interest/curiosity */}
-        <a
-          href={`http://maps.google.com/maps?q=${station.lat}+${station.lon}&ll=${station.lat},${station.lon}&spn=0.004250,0.011579&t=h&iwloc=A&hl=en`}
-        >
-          Vis
-        </a>
-      </td>
-    </tr>
-  ));
-
-  const dataTable = (
-    <table>
-      <thead>
-        <tr>
-          <td>Oslo Bysykkel: Stasjoner</td>
-          <td>Ledige sykler</td>
-          <td>Ledige låser</td>
-          <td>Kart</td>
-        </tr>
-      </thead>
-      <tbody>{dataTableRows}</tbody>
-    </table>
-  );
-  console.log(updateTime);
+  // exp = fetchData;
 
   return (
     <div className="app">
-      <Kart style={{marginTop: '20px'}} stationsWithStatus={stationsWithStatus} />
-      <div className="oppdatert">Oslo Bysykkel: Ledige sykler og låser. 
-        Oppdatert kl {updateTime.toTimeString().substr(0,5)}
+      <BikeMap stationsWithStatus={stationsWithStatus} />
+      <div className="info-text">Oslo Bysykkel: Ledige sykler og låser. 
         <div>Hold markøren over stasjonene for å se hvor mange sykler og låser som er ledige.</div>
+        <div>Oppdatert klokken <time dateTime={String(updateTime)}>{String(updateTime)}</time></div>
       </div>
     </div>
   );
---
+++ src/BikeMap.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+
+const BikeMap = ({ stationsWithStatus, google }) => {
+  // Limit map view to stations
+  var bounds = new google.maps.LatLngBounds();
+
+  const stations = [...stationsWithStatus.values()].map(
+    ({ name, lat, lon, num_bikes_available, num_docks_available }) => ({
+      name,
+      lat,
+      lon,
+      bikes: num_bikes_available,
+      locks: num_docks_available
+    })
+  );
+
+  const markers = stations.map((station, i) => {
+    const pos = { lat: station.lat, lng: station.lon };
+    // Include station in map view
+    bounds.extend(pos);  
+    
+    const bikeText = station.bikes === 1 ? 'sykkel' : 'sykler';
+    const lockText = station.locks === 1 ? 'lås' : 'låser';
+    
+    return (
+      <Marker
+        key={i}
+        position={pos}
+        title={`${station.name}: ${station.bikes} ${bikeText}, ${station.locks} ${lockText}`}
+      ></Marker>)
+  });
+
+  // Data attribute is not applied by the map component. It is the preferred selector for testing.
+  return (
+    <Map
+      className='bike-map'
+      data-test='bike-map' 
+      google={google}
+      initialCenter={{ lat: 59.92, lng: 10.74}}
+      bounds={bounds}
+    >
+    {markers}
+    </Map>
+  );
+};
+
+export default GoogleApiWrapper({
+  // In a production app, this api key should be set in env. 
+  // Maybe set env apikey when running .
+  // https://www.rockyourcode.com/secret-keys-in-react 
+  apiKey: "AIzaSyC2Z0xQIg8y3Nv_qtgb1yRGPN3kSjHfVQ4"
+})(BikeMap);
