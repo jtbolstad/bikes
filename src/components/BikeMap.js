34a19f926b5232bfb3e703c98ed45a084799a09e
Jan Thure Bolstad <jtbolstad@gmail.com>
2019-09-22 14:22:47 +0200

    Remove two files

--- src/components/BikeMap.js
+++ src/components/BikeMap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
-import config from '../config.js';
+import config from "../config.js";
 
 const BikeMap = ({ stationsWithStatus, google }) => {
   // Limit map view to stations
@@ -19,36 +19,35 @@ const BikeMap = ({ stationsWithStatus, google }) => {
   const markers = stations.map((station, i) => {
     const pos = { lat: station.lat, lng: station.lon };
     // Include station in map view
-    bounds.extend(pos);  
-    
-    const bikeText = station.bikes === 1 ? 'sykkel' : 'sykler';
-    const lockText = station.locks === 1 ? 'lås' : 'låser';
-    
+    bounds.extend(pos);
+
+    const bikeText = station.bikes === 1 ? "sykkel" : "sykler";
+    const lockText = station.locks === 1 ? "lås" : "låser";
+
     return (
       <Marker
         key={i}
         position={pos}
         title={`${station.name}: ${station.bikes} ${bikeText}, ${station.locks} ${lockText}`}
-      ></Marker>)
+      ></Marker>
+    );
   });
 
   // Data attribute is not applied by the map component. It is the preferred selector for testing.
   return (
     <Map
-      className='bike-map'
-      data-test='bike-map' 
+      className="bike-map"
+      data-test="bike-map"
       google={google}
-      initialCenter={{ lat: 59.92, lng: 10.74}}
+      initialCenter={{ lat: 59.92, lng: 10.74 }}
       bounds={bounds}
     >
-    {markers}
+      {markers}
     </Map>
   );
 };
 
 export default GoogleApiWrapper({
-  // In a production app, this api key should be set in env. 
-  // Maybe set env apikey when running .
-  // https://www.rockyourcode.com/secret-keys-in-react 
+  // In a production app, this api key should be set in env.
   apiKey: config.googleMapsApiKey
 })(BikeMap);
